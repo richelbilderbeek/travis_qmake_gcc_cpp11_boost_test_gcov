9fef2819262eaf412ba49a8c88b89bc3b4c1598c
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-02 12:14:40 +0200

    Minimal project

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 qmake travis_qmake_gcc_cpp11_boost_test_gcov.pro
-make debug
-./travis_qmake_gcc_cpp11_boost_test_gcov
+make
--- build_normal_release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-qmake travis_qmake_gcc_cpp11_boost_test_gcov.pro
-make release
-./travis_qmake_gcc_cpp11_boost_test_gcov
